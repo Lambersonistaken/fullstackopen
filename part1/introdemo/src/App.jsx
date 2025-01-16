e374d18e9fbb77b5b25009f558a4c9775189de6a
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2025-01-16 22:52:55 +0300

    done

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,13 +1,10 @@
 /* eslint-disable react/prop-types */
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+const Hello = (props) => {
+
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
+      <p>Hello World for {props.name} and you are {props.age} years old</p>
     </div>
   )
 }
@@ -31,7 +28,7 @@ const App = () => {
       <div>
         <h1>Hello Worl it is {now.toString()}</h1>
         <p>{a} plus {b} equals {a+b}</p>
-        <Hello name="Maya" age={26 + 10} />
+        <Hello name="Emir" age={a+b}/>
       </div>
     </>
   )
