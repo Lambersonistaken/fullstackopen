56d4ef1206bfa70755509196d00eaf7166afe2e4
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2025-01-16 22:17:32 +0300

    update

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
 
+
+
+
+
+
+
+
+const App = () => {
+
+  const now = new Date()
+  const a = 10;
+  const b = 20;
+  console.log(now, a+b);
+  
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello Worl it is {now.toString()}</h1>
+        <p>{a} plus {b} equals {a+b}</p>
+        <Hello name="Maya" age={26 + 10} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
