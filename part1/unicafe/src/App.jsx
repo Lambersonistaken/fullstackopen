3e6cedb240f37c807add3fcb1d176e5926454533
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2025-01-20 17:25:23 +0300

    part1 exercise 1.7done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,19 +5,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
 
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
+  
+
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
 
   return (
     <div>
@@ -34,6 +42,9 @@ const App = () => {
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
         <p>Bad: {bad}</p>
+        <p>All: {all}</p>
+        <p>Average: {average}</p>
+        <p>Positive: {positive} %</p>
       </div>  
     </div>
   )
