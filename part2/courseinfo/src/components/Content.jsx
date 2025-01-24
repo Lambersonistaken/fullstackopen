95a2de69a009ba6687eadecfed6246183e71ecfe
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2025-01-24 22:47:20 +0300

    part2 exercise 2.1,2.2 done

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -4,9 +4,7 @@ import Part from "./Part"
 const Content = (props) => {
   return (
     <div>
-        <Part partcount={props.parts[0].name} exercisecount={props.parts[0].exercises} />
-        <Part partcount={props.parts[1].name} exercisecount={props.parts[1].exercises} />
-        <Part partcount={props.parts[2].name} exercisecount={props.parts[2].exercises} />
+        {props.parts.map(part => <Part key={part.id} partcount={part.name} exercisecount={part.exercises} />)}
     </div>
   )
 }
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,13 +1,13 @@
 import Content from "./Content"
 import Header from "./Header"
-// import Total from "./Total"
+import Total from "./Total"
 
 const Course = (props) => {
   return (
     <div>
       <Header course={props.course} />
       <Content parts={props.course.parts} />
-      {/* <Total parts={props.course.parts} /> */}
+      <Total parts={props.course.parts} />
     </div>
   )
 }
