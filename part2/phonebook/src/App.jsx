f43c023e0d07d97bd0f96f363479c8cbe3047fc0
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2025-02-06 21:41:44 +0300

    2.9 update

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-123456'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -35,7 +36,8 @@ const App = () => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(personObject))
     setNewName('')
@@ -61,7 +63,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {search === '' ? persons.map(person => <p key={person.name}>{person.name} {person.number}</p>) : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())).map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {search === '' ? persons.map(person => <p key={person.id}>{person.name} {person.number}</p>) : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())).map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
