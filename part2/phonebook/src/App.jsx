d6a9e286733fd5bc4665781fb99ade39c2b3b133
Lambersonistaken <lambersonistaken@gmail.com>
2025-11-26 21:10:49 +0300

    part2 exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,65 +1,68 @@
-import { useState } from 'react'
-import PersonForm from './components/PersonForm'
-import Filter from './components/Filter'
-import Persons from './components/Persons'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [search, setSearch] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
-
-const handleSubmit = (event) => {
-
-    if(persons.find(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+  const handleSubmit = (event) => {
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
       return;
     }
 
-
-    event.preventDefault()
+    event.preventDefault();
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
-    }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-  }
-
-  
-
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
   const handleSearchChange = (event) => {
-    setSearch(event.target.value)
-  }
-
-  
+    setSearch(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <PersonForm newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} handleSubmit={handleSubmit} />
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleSubmit={handleSubmit}
+      />
       <Filter handleSearchChange={handleSearchChange} search={search} />
       <h2>Numbers</h2>
       <Persons search={search} persons={persons} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
