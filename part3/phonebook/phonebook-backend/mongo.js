a91cd1eb69ff41c864e4ce5c0066654b9d8a57c5
Lambersonistaken <lambersonistaken@gmail.com>
2026-07-01 15:56:32 +0300

    exercise 3.20 done

--- part3/phonebook/phonebook-backend/mongo.js
+++ part3/phonebook/phonebook-backend/mongo.js
@@ -23,7 +23,17 @@ const noteSchema = new mongoose.Schema({
         minLength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /\d{2,3}-\d+/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: true,
+    },
 })
 
 const Note = mongoose.model('Note', noteSchema)
